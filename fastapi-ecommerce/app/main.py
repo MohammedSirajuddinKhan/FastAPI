25055c2ff39dd3a862e8d3db35ad8a9cad1803f8
Sirajuddin Khan <sirajuddinkhan7718@gmail.com>
2026-09-17 17:25:31 +0530

    basic rest fastapi created

--- fastapi-ecommerce/app/main.py
+++ fastapi-ecommerce/app/main.py
@@ -1,12 +1,80 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, Field
+from dotenv import load_dotenv
+import os
 
-app=FastAPI()
+load_dotenv()
+
+app = FastAPI(
+    title=os.getenv("APP_NAME", "Taskflow API"),
+    version=os.getenv("APP_VERSION", "1.0.0")
+)
+
+
+class TaskCreate(BaseModel):
+    title: str = Field(min_length=3, max_length=100)
+    description: str = Field(min_length=5, max_length=500)
+    priority: int = Field(ge=1, le=5)
+
+
+class TaskUpdate(BaseModel):
+    title: str | None = Field(None, min_length=3, max_length=100)
+    description: str | None = Field(None, min_length=5, max_length=500)
+    priority: int | None = Field(None, ge=1, le=5)
+    completed: bool | None = None
+
+
+tasks = []
+
 
 @app.get('/')
-def root():
-    return {"Message":"Is ts tuff..?"}
+def home():
+    return {"message": "Welcome to TaskFlow API"}
 
-@app.get('/products/{id}')
-def get_products(id:int):
-    products=['tws','monitor','laptop','mouse']
-    return products[id]
+
+@app.get('/health')
+def get_health():
+    return {"status": "healthy"}
+
+
+@app.get('/tasks')
+def get_tasks():
+    return tasks
+
+
+@app.get('/tasks/{task_id}')
+def get_task(task_id: int):
+    for task in tasks:
+        if task["id"] == task_id:
+            return task
+    raise HTTPException(404, "Task Not Found")
+
+
+@app.post('/tasks')
+def create_task(task: TaskCreate):
+    new_task = {
+        "id": len(tasks)+1,
+        "title": task.title,
+        "description": task.description,
+        "priority": task.priority,
+        "completed": False
+    }
+    tasks.append(new_task)
+    return new_task
+
+
+@app.put('/tasks/{task_id}')
+def update_task(task_id: int, data: TaskUpdate):
+    for task in tasks:
+        if task["id"] == task_id:
+            task.update(data.model_dump(exclude_unset=True))
+            return task
+    raise HTTPException(404,"Task not found")
+
+@app.delete("/tasks/{task_id}")
+def delete_task(task_id:int):
+    for i, task in enumerate(tasks):
+        if task["id"] == task_id:
+            return tasks.pop(i)
+
+    raise HTTPException(404,"Task not found")
